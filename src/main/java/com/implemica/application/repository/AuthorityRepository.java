ec8ed174be9cec4433ad61ac06b2bddf9ac997cf
xenia.s <xenia.s@implemica.com>
2023-02-14 19:14:11 +0200

    Initial version of carCatalog generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/implemica/application/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.implemica.application.repository;
+
+import com.implemica.application.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
