c8c92da852cbe2f5fc2cceb1033ada053a174e7b
xenia.s <xenia.s@implemica.com>
2023-03-06 12:53:42 +0200

    Initial version of authentication generated by generator-jhipster@7.9.3

---
+++ authentication/src/main/java/com/implemica/authentication/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.implemica.authentication.repository;
+
+import com.implemica.authentication.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ authentication/src/test/java/com/implemica/authentication/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.implemica.authentication;
+
+import com.implemica.authentication.AuthenticationApp;
+import com.implemica.authentication.config.AsyncSyncConfiguration;
+import com.implemica.authentication.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { AuthenticationApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
